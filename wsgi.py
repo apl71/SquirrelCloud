09318e8e9751ae83ee71bc4e51ae20ecca03edc1
Yingjie Lu <luyingjie1998@outlook.com>
2024-12-09 17:09:05 +0000

    Move default port to 5000.

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     ssl_context = ("cert/cert.pem", "cert/key.pem")
-    app.run(debug=True, host="0.0.0.0", port=443, ssl_context=ssl_context)
+    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context=ssl_context)
