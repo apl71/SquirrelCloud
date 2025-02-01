d35a9e077f63d0bdd762eeb005e9e9e15b4e4dc7
Yingjie Lu <luyingjie1998@outlook.com>
2025-02-01 08:11:40 +0000

    New UI: table for external link.

--- static/js/function.js
+++ static/js/function.js
@@ -1,5 +1,6 @@
 function load_function() {
     load_theme();
+    load_external_links();
 }
 
 function find_replicas() {
@@ -72,4 +73,88 @@ function find_replicas() {
             });
         });
     })
+}
+
+async function get_host() {
+    const response = await fetch("/api/host", {
+        method: "GET"
+    });
+    const result = await response.json();
+    if (result["result"] != "OK") {
+        alert("Failed to get host.");
+        return "HOST";
+    }
+    return result["host"];
+}
+
+async function load_external_links() {
+    const host = await get_host();
+    fetch("/api/all_external_links", {
+        method: "GET"
+    }).then(
+        response => response.json()
+    ).then(result => {
+        if (result["result"] != "OK") {
+            alert("Failed to get external links.");
+            return;
+        }
+        const external_links = result["links"];
+        const table = document.getElementById("external_link_table");
+        table.innerHTML = "";
+        // table header
+        const thead = document.createElement("thead");
+        const header_row = document.createElement("tr");
+        const th1 = document.createElement("th");
+        th1.textContent = "Path";
+        header_row.appendChild(th1);
+        const th2 = document.createElement("th");
+        th2.textContent = "Link";
+        header_row.appendChild(th2);
+        const th3 = document.createElement("th");
+        th3.textContent = "Expire Time";
+        header_row.appendChild(th3);
+        const th4 = document.createElement("th");
+        th4.textContent = "Remove";
+        header_row.appendChild(th4);
+        thead.appendChild(header_row);
+        table.insertBefore(thead, table.firstChild);
+        // table body
+        const tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+        tbody.innerHTML = "";
+        external_links.forEach(link => {
+            const row = tbody.insertRow(-1);
+            const path_cell = row.insertCell(0);
+            path_cell.innerHTML = link["path"];
+            const url_cell = row.insertCell(1);
+            const url = document.createElement("a");
+            url_cell.appendChild(url);
+            url.innerHTML = "https://" + host + "/api/external_link?key=" + link["key"];
+            url.href = url.innerHTML;
+            const expire_cell = row.insertCell(2);
+            expire_cell.innerHTML = link["expire"];
+            const remove_cell = row.insertCell(3);
+            remove_cell.className = "center_cell";
+            const remove_button = document.createElement("button");
+            remove_cell.appendChild(remove_button);
+            remove_button.textContent = "X";
+            remove_button.onclick = function() {
+                delete_external_link(link["key"]);
+                load_external_links();
+            };
+        });
+    });
+}
+
+function delete_external_link(key) {
+    fetch("/api/remove_external_link?key=" + key, {
+        method: "DELETE"
+    }).then(
+        response => response.json()
+    ).then(result => {
+        if (result["result"] != "OK") {
+            alert("Failed to delete external link.");
+            return;
+        }
+    });
 }
